1de2c6fd4c1c2a714b817b4067ebddac61e2d4ba
Andre Girao <andrelgirao22@hotmail.com>
2018-07-15 16:30:04 -0300

    Configurando o viewresolver

--- src/main/java/com/alg/brewer/controller/CervejasController.java
+++ src/main/java/com/alg/brewer/controller/CervejasController.java
@@ -1,8 +1,14 @@
 package com.alg.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
 
+	@RequestMapping("/cerveja/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
 }
