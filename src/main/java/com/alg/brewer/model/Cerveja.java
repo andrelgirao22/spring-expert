572120d3b637b44dd82830aebb7d904af108b134
Andre Girao <andrelgirao22@hotmail.com>
2018-07-18 13:34:43 -0300

    resolvendo problemas de acentuação

--- src/main/java/com/alg/brewer/model/Cerveja.java
+++ src/main/java/com/alg/brewer/model/Cerveja.java
@@ -1,12 +1,19 @@
 package com.alg.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 
 	@NotBlank
 	private String sku;
+	
+	@NotBlank
 	private String nome;
+	
+	@Size(min=1, max=30)
+	private String descricao;
 	
 	public String getSku() {
 		return sku;
@@ -20,4 +27,16 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public String toString() {
+		return "Cerveja [sku=" + sku + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+	
+	
 }
