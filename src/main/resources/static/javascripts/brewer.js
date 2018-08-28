66fc76ef1bb2810778a085b6396f4f6fae4f768c
Andre Girao <andrelgirao22@hotmail.com>
2018-08-28 14:22:27 -0300

    convertendo valores para o formato brasileiro

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -2,10 +2,10 @@
 $(function() {
 	
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
-	//decimal.maskMoney({decimal:',',thousands:'.'});
+	//decimal.maskMoney();
+	decimal.maskMoney({decimal:',',thousands:'.'});
 	
 	var plain = $('.js-plain');
-	plain.maskMoney({precision: 0});
+	plain.maskMoney({precision: 0, thousands:'.'});
 	
 })
